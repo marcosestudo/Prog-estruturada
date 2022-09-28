5c0a83fba8808d88815f4ce1a54fcaac37a515db
Marcos Fernando <estudogit@outlook.com>
2022-09-28 14:30:25 -0300

    fib recursivo retornando enesimo elemento

---
+++ 012_fib_recursivo_retornando_enesimo_elemento/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int fibonnacci(int n)                            
+{                                         
+if (n == 1) return 1;                   
+  else                                    
+if (n == 2) return 1;                 
+  else return fibonnacci(n - 1) + fibonnacci(n - 2);  
+} 
+
+int main(void) {
+  float n;
+
+  do {
+    printf("Digite um número positivo: ");
+    scanf("%f", &n);    
+  } while (n <= 0);  
+
+  printf("%d\n", fibonnacci(n));
+  
+  return 0;
+}
