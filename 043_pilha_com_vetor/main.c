030b1daf7b0013d8689d3bf4319adfade1c37198
Marcos Fernando <estudogit@outlook.com>
2022-11-02 20:55:25 -0300

    pilha com vetor

---
+++ 043_pilha_com_vetor/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define MAX 5
+
+struct PILHA {
+  int dado[MAX];
+  int topo;
+};
+typedef struct PILHA pilha;
+
+void push (pilha *P, int elemento) {
+  if (P -> topo < MAX -1) {
+    P -> topo += 1;
+    P -> dado[P -> topo] = elemento;
+  } else {
+    printf("Pilha cheia\n");
+  }  
+}
+
+int pop (pilha *P) {
+  int aux;
+  if (P -> topo > -1) {
+    aux = P -> dado [P -> topo];
+    P -> topo -= 1;
+  } else {
+    printf("Pilha vazia");
+  }
+  return aux;
+}
+
+void print (pilha P) {
+  int i;
+  if (P.topo > -1) {
+    for (i = 0; i <= P.topo; i ++) {
+      printf("%d ", P.dado[i]);
+    }
+  } else {
+    printf("Pilha vazia");
+  }
+}
+
+int main (void) {
+  pilha P;
+  // inicializando o topo da pilha com -1 pra sinalizar que ela está vazia
+  P.topo = -1; 
+
+  push(&P, 1);
+  push(&P, 9);
+  push(&P, 4);
+  push(&P, 3);
+  push(&P, 19);
+  push(&P, 43);
+
+  print(P);
+  printf("\n");
+  
+  printf("%d\n", pop(&P));
+  printf("%d\n", pop(&P));
+  
+  print(P);
+  
+  return 0;
+}
