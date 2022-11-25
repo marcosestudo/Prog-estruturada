15e8071ee9dd10e8cb09ffdfde55b4f39f3a0551
Marcos Fernando <estudogit@outlook.com>
2022-11-25 17:28:30 -0300

    prototipos adicionados

--- 062_lista_prototipos/main.c
+++ 062_lista_prototipos/main.c
@@ -2,29 +2,63 @@
 #include <stdlib.h>
 
 struct NO {
-    int info;    
-    struct NO *prox;
+	int info;
+	struct NO *prox;
 };
 typedef struct NO lista;
 
-void mostra(lista *L);
+//  ___ ___  ___ _____ ___ _____ ___ ___  ___  ___ 
+// | _ \ _ \/ _ \_   _/ _ \_   _|_ _| _ \/ _ \/ __|
+// |  _/   / (_) || || (_) || |  | ||  _/ (_) \__ \
+// |_| |_|_\\___/ |_| \___/ |_| |___|_|  \___/|___/
+int busca(lista *L, int elem, lista *pre);
 
-int busca(lista *L, int elem, lista **pre);
+void mostra(lista *L);
 
 void insere1(lista **L, int elem);
 lista *insere2(lista *L, int elem);
+void insereNoInicio1(lista **L, int elem);
+lista *insereNoInicio2(lista *L, int elem);
+void insereNofim1(lista **L, int elem);
+lista *insereNofim2(lista *L, int elem);
 
 void remove1(lista **L, int elem);
 lista *remove2(lista *L, int elem);
 int removePrimeiro(lista **L, int *removido);
 int removeUltimo(lista **L, int *removido);
 
- //  __  __     _     ___   _  _ 
- // |  \/  |   /_\   |_ _| | \| |
- // | |\/| |  / _ \   | |  | .` |
- // |_|  |_| /_/ \_\ |___| |_|\_|
+// concatena
+// inverte
+// intercala
+// interseccao
+// uniao
+// diferenca
+// ordena
+
+//  __  __     _     ___   _  _
+// |  \/  |   /_\   |_ _| | \| |
+// | |\/| |  / _ \   | |  | .` |
+// |_|  |_| /_/ \_\ |___| |_|\_|
 int main(void) {
-    lista *L;
-    
-    return 0;
+	lista *L;
+
+	L = NULL;
+
+	return 0;
+}
+
+//  ___ _   _ _  _  ___ ___  ___ ___ 
+// | __| | | | \| |/ __/ _ \| __/ __|
+// | _|| |_| | .` | (_| (_) | _|\__ \
+// |_|  \___/|_|\_|\___\___/|___|___/
+void mostra(lista *L) {
+	if (L != NULL) {
+		printf("%d|| ", L->info);
+		if (L->prox != NULL) {
+			mostra(L - prox);
+		}
+	}
+}
+
+int busca(lista *L, int elem, lista **pre) {
 }
