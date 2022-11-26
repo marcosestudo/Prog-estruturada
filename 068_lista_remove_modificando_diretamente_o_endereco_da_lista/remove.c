34eafcb18c3d2a8287cbcdb82f3fd79d3fbc61a9
Marcos Fernando <estudogit@outlook.com>
2022-11-26 20:45:42 -0300

    lista remove modificando diretamente o endereco da lista

---
+++ 068_lista_remove_modificando_diretamente_o_endereco_da_lista/remove.c
@@ -0,0 +1,17 @@
+
+void remove1(lista **L, int elem) {
+	lista *pre, *lixo;
+	if (busca((*L), elem, &pre)) {
+		if ((*L)->info == elem) {
+			lixo = (*L);
+			(*L) = lixo->prox;
+		} else {
+			lixo = pre->prox;
+			pre->prox = lixo->prox;
+		}
+		free(lixo);
+		printf("\nRemovido: %d", elem);
+	} else {
+		printf("\nElemento nao encontrado: %d", elem);
+	}
+}
